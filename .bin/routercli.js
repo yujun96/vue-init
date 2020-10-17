678f0080f655b3baa187435ac8d85bb0f4b21362
yujun <363911532@qq.com>
2020-10-17 16:13:44 +0800

    优化

---
+++ .bin/routercli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node 
+const program = require('commander')
+program.version(require('../package.json').version)
+program.command('init <name>')
+       .description('init project')
+       .action(require('../lib/init.js'))
+program.parse(process.argv)
